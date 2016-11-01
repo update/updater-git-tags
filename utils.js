4931b4e45d51043c20dee23409837b425f205ca0
doowb <brian.woodward@gmail.com>
2016-11-01 11:58:50 -0400

    pass commit hash to `git tag` command to ensure correct hash is tagged use `git log` with pretty formatter directly

--- utils.js
+++ utils.js
@@ -17,14 +17,42 @@ require('extend-shallow', 'extend');
 require('fs-exists-sync', 'exists');
 require('is-valid-app', 'isValid');
 require('log-utils', 'log');
-require('parse-git-log');
 require = fn;
 
 utils.parseLog = function(options, cb) {
   var re = /^(v?\d*\.\d*\.\d*)/i;
-  utils.parseGitLog(options)
-    .then(function(results) {
-      results = results.filter(function(commit) {
+  var cwd = process.cwd();
+  if (typeof options === 'function') {
+    cb = options;
+    options = {};
+  }
+
+  var opts = utils.extend({cwd: cwd}, options);
+  var fp = path.join(opts.cwd, '.git');
+  var results = [];
+  if (utils.exists(fp)) {
+    process.chdir(fp);
+    // formats logs to look like
+    // 793cfa5db68794e5f77951f9e5abb4e9ef6cd41a first commit
+    var child = utils.spawn.sync('git', ['log', '--pretty=%H %s']);
+    var output = child.stdout.toString();
+    process.chdir(cwd);
+
+    results = output
+      .split('\n')
+      .filter(Boolean)
+      .map(function(line) {
+        var idx = line.indexOf(' ');
+        if (idx === -1) {
+          return;
+        }
+
+        return {
+          commit: line.slice(0, idx),
+          message: line.slice(idx + 1)
+        };
+      }).filter(Boolean)
+      .filter(function(commit) {
         if (!commit || !commit.message) {
           return false;
         }
@@ -34,10 +62,9 @@ utils.parseLog = function(options, cb) {
           return true;
         }
       });
-      cb(null, results);
-    }, function(err) {
-      cb(err);
-    });
+  }
+
+  cb(null, results);
 };
 
 utils.parseTags = function(options, cb) {
@@ -72,7 +99,7 @@ utils.tagCommit = function(commit, options, cb) {
   var fp = path.join(opts.cwd, '.git');
   if (utils.exists(fp)) {
     process.chdir(fp);
-    var results = utils.spawn.sync('git', ['tag', '-a', commit.message, '-m', commit.message]);
+    var results = utils.spawn.sync('git', ['tag', '-a', commit.message, '-m', commit.message, commit.commit]);
     process.chdir(cwd);
 
     if (results.error || (results.stderr && results.stderr.toString())) {
