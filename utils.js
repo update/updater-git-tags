3b087964619d1062b94f533ffc348250a729d33f
jonschlinkert <jon.schlinkert@sellside.com>
2017-04-29 01:01:42 -0400

    minor code changes

--- utils.js
+++ utils.js
@@ -18,95 +18,75 @@ require('log-utils', 'log');
 require = fn;
 
 utils.parseLog = function(options, cb) {
-  var re = /^(v?\d*\.\d*\.\d*)/i;
   var cwd = process.cwd();
+
   if (typeof options === 'function') {
     cb = options;
-    options = {};
+    options = undefined;
   }
 
   var opts = utils.extend({cwd: cwd}, options);
   var fp = path.join(opts.cwd, '.git');
   var results = [];
+
   if (fs.existsSync(fp)) {
     process.chdir(fp);
     // formats logs to look like
     // 793cfa5db68794e5f77951f9e5abb4e9ef6cd41a first commit
     var child = utils.spawn.sync('git', ['log', '--pretty=%H %s']);
-    var output = child.stdout.toString();
+    var gitLog = child.stdout.toString();
     process.chdir(cwd);
 
-    results = output
-      .split('\n')
-      .filter(Boolean)
-      .map(function(line) {
-        var idx = line.indexOf(' ');
-        if (idx === -1) {
-          return;
+    var regex = /^(v?\d+\.\d+(\.\d+)?)/i;
+    var commits = gitLog.split('\n');
+    for (var i = 0; i < commits.length; i++) {
+      var commit = commits[i];
+      if (commit && commit.trim()) {
+        var segs = commit.split(' ');
+        if (regex.test(segs[1])) {
+          if (segs.length === 2) {
+            results.push({commit: segs[0], message: segs[1]});
+          }
         }
-
-        return {
-          commit: line.slice(0, idx),
-          message: line.slice(idx + 1)
-        };
-      }).filter(Boolean)
-      .filter(function(commit) {
-        if (!commit || !commit.message) {
-          return false;
-        }
-
-        var m = commit.message.match(re);
-        if (m && m[1]) {
-          return true;
-        }
-      });
+      }
+    }
   }
 
   cb(null, results);
 };
 
 utils.parseTags = function(options, cb) {
+  git(['tag'], options, cb);
+};
+
+utils.tagCommit = function(commit, options, cb) {
+  var msg = commit.message;
+  var commands = ['tag', '-a', commit.message, '-m', commit.message, commit.commit];
+  git(commands, options, cb);
+};
+
+function git(commands, options, cb) {
   var cwd = process.cwd();
   if (typeof options === 'function') {
     cb = options;
-    options = {};
+    options = undefined;
   }
-
   var opts = utils.extend({cwd: cwd}, options);
   var fp = path.join(opts.cwd, '.git');
   var res = [];
   if (fs.existsSync(fp)) {
     process.chdir(fp);
-    var child = utils.spawn.sync('git', ['tag']);
-    res = child.stdout.toString()
-      .split('\n')
-      .filter(Boolean);
+    var child = utils.spawn.sync('git', commands);
+    res = child.stdout.toString();
+    if (res.error || (res.stderr && res.stderr.toString())) {
+      cb(new Error(res.error || res.stderr.toString()));
+      return;
+    }
+    res = res.split('\n').filter(Boolean);
     process.chdir(cwd);
   }
   cb(null, res);
-};
-
-utils.tagCommit = function(commit, options, cb) {
-  var cwd = process.cwd();
-  if (typeof options === 'function') {
-    cb = options;
-    options = {};
-  }
-
-  var opts = utils.extend({cwd: cwd}, options);
-  var fp = path.join(opts.cwd, '.git');
-  if (fs.existsSync(fp)) {
-    process.chdir(fp);
-    var results = utils.spawn.sync('git', ['tag', '-a', commit.message, '-m', commit.message, commit.commit]);
-    process.chdir(cwd);
-
-    if (results.error || (results.stderr && results.stderr.toString())) {
-      cb(new Error(results.error || results.stderr.toString()));
-      return;
-    }
-  }
-  cb(null, (results.stdout && results.stdout.toString()) || '');
-};
+}
 
 /**
  * Expose `utils` modules
